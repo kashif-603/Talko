454f5c45ec42817cc54278cbeb0efc93d0497bc9
kashif-603 <kashif8315@gmail.com>
2025-05-17 13:03:52 +0500

    Revert "first"
    This reverts commit a13a41ae134c6a60884a66774bf8d866e7acd6df.

--- android/app/src/main/kotlin/com/example/talko_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.talko_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/config/colors.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:ui';
-
-const dBackgroundColor=Color(0xff191828);
-const PrimaryColor=Color(0xffFFA500);
-const dContainerColor=Color(0xff343748);
-const donBackgroundColor=Color(0xffffffff);
-const donContainerColor=Color(0xff9E9E9E);
